4b42cf36e14473810229b1a6f2efe24f554b8471
wlonep <frin6189@gmail.com>
2025-05-15 18:12:22 +0900

    -

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'), http = require('http'), path = require('path');
+const static = require('serve-static');
+
+const app = express();
+const router = express.Router();
+
+app.set('port', process.env.PORT || 8080);
+app.set('host', '127.0.0.1');
+app.use(static(__dirname));
+app.use(express.urlencoded());
+app.use(express.json());
+
+http.createServer(app).listen(app.get('port'), app.get('host'), () => {
+   console.log('Server started on http://' + app.get('host') + ':' + app.get('port') + '...');
+});
