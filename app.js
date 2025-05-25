9e0b21c6a9a37f9889f4a8bca0e41022fe014def
wlonep <frin6189@gmail.com>
2025-05-25 18:41:01 +0900

    -

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.use(static(__dirname));
 app.use(express.urlencoded());
 app.use(express.json());
 
+router.route('/').get((req, res) => {
+   res.redirect('/source/jquery.html')
+});
+
+router.route('/routetest').get((req, res) => {
+   res.redirect('https://google.com/')
+});
+
+app.use('/', router);
+
 http.createServer(app).listen(app.get('port'), app.get('host'), () => {
    console.log('Server started on http://' + app.get('host') + ':' + app.get('port') + '...');
 });
