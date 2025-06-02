bb0662941749d158d884569f1c1f73a656d91575
Jiwon Park <98960141+wlonep@users.noreply.github.com>
2025-06-02 16:33:41 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express'), http = require('http'), path = require('path');
+const https = require('https');
+const fs = require("fs");
 const static = require('serve-static');
+
+const options = {
+   key: fs.readFileSync("cert.key"),
+   cert: fs.readFileSync("cert.crt")
+};
 
 const app = express();
 const router = express.Router();
 
 app.set('port', process.env.PORT || 8080);
-app.set('host', '127.0.0.1');
+app.set('host', '172.16.164.77');
 app.use(static(__dirname));
 app.use(express.urlencoded());
 app.use(express.json());
@@ -18,8 +25,41 @@ router.route('/routetest').get((req, res) => {
    res.redirect('https://google.com/')
 });
 
+router.route('/rss').get((req, res) => {
+   console.log("RSS data requested");
+   const feed = 'https://news.sbs.co.kr/news/headlineRssFeed.do?plink=RSSREADER';
+   https.get(feed, (httpRes) => {
+      let rssRes = "";
+      httpRes.on('data', (chunk) => rssRes += chunk);
+      httpRes.on('end', () => {
+         res.send(rssRes);
+         console.log("RSS respond completed");
+         res.end();
+      })
+   })
+});
+
+router.route('/youtube-rss').get((req, res) => {
+   console.log("RSS data requested");
+   const feed = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCRWxH4fGhuNsh0klWnDbt0w';
+   https.get(feed, (httpRes) => {
+      let rssRes = "";
+      httpRes.on('data', (chunk) => rssRes += chunk);
+      httpRes.on('end', () => {
+         res.send(rssRes);
+         console.log("RSS respond completed");
+         res.end();
+      })
+   })
+})
+
 app.use('/', router);
 
 http.createServer(app).listen(app.get('port'), app.get('host'), () => {
-   console.log('Server started on http://' + app.get('host') + ':' + app.get('port') + '...');
+   console.log(`Server started on http://${app.get('host')}:${app.get('port')}`);
 });
+
+const PORT = 8000;
+https.createServer(options, app).listen(PORT, app.get('host'), () => {
+   console.log(`Express HTTPS started on https://${app.get('host')}:${PORT}`)
+})
---
+++ jquery.js
@@ -0,0 +1,185 @@
+let i = 0;
+let imgCounter = 0;
+const imgArray = ["img1.png", "img2.png", "img3.png", "img4.png", "img5.png"];
+
+$(document).ready(() => {
+    $("div.out").mouseover(() => {
+        $("div.out p:first-child").text("mouse over");
+        $("div.out p:nth-child(2)").text(i);
+        i++;
+    }).mouseout(() => {
+        $("div.out p:first-child").text("mouse out");
+    })
+
+    $("#b1").on("click",
+        {
+            url: "https://www.google.com",
+            winattributes: "resize=1, scrollbars=1, status=1"
+        },
+        max_open
+    );
+
+    $("#bind").click(() => {
+        $("body").on("click", "#theone", flash)
+            .find("#theone")
+            .text("Can Click!");
+    });
+
+    $("#unbind").click(() => {
+        $("body").off("click", "#theone", flash)
+            .find("#theone")
+            .text("Does Nothing...");
+    });
+
+    $("#trigger_test button:first").click(() => update($("#trigger_test span:first")));
+    $("#trigger_test button:last").click(() => {
+        $("#trigger_test button:first").trigger("click");
+        update($("#trigger_test span:last"));
+    });
+
+    $("#image").click((e) => {
+        const newSrc = $(e.target).attr("src") === "img1.png" ? "img2.png" : "img1.png";
+        $(e.target).attr("src", newSrc);
+    });
+
+    $("#imgAlbum")
+        .attr("src", imgArray[imgCounter])
+        .click(() => {
+        imgCounter = (imgCounter + 1) % imgArray.length;
+        $("#imgAlbum").attr("src", imgArray[imgCounter]);
+    });
+
+    $("#add_img img").click(() => {
+        $("#note_form").fadeIn().addClass("popup");
+        change_position($(".popup"));
+    });
+
+    $("#add_note").click(() => {
+        $("#note_form").fadeOut(() => {$(this).removeClass("popup")});
+        $("#note").append(`${$("#note_title").val()}<br/>${$("#note_date").val()}<br/>${$("#note_content").val()}<br/><br/>`);
+    });
+
+    $(window).resize(() => {
+        change_position($(".popup"));
+    });
+
+    $("#moving_button").click(() => {
+        const width = $("#moving_box").css("width");
+        if (parseInt(width) + 50 >= parseInt($("#animation_test").css("width"))) return;
+        $("#moving_box").animate({"right": 0, 
+            "width": "+=50px", 
+            "height": "+=50px"});
+        $("#animation_test").css({"height": "+=50px"})
+    });
+
+    $(".accordion").each(function() {
+        const dds = $(this).find("dd");
+        const dts = $(this).find("dt");
+        function closeAll() {
+            dds.addClass("closed");
+            dts.addClass("closed");
+        }
+        function open(dt, dd) {
+            dt.removeClass("closed");
+            dd.removeClass("closed");
+        }
+        closeAll();
+        dts.click((e) => {
+            closeAll();
+            open($(e.target), $(e.target).next());
+        });
+    })
+
+    $('.slideshow').each(function() {
+        const container = $(this);
+        let timer;
+        function switchImg() {
+            const imgs = container.find("img");
+            const first = imgs.eq(0);
+            const second = imgs.eq(1);
+            first.appendTo(container).fadeOut(2000);
+            second.fadeIn();
+        }
+        function startTimer() {
+            timer = setInterval(switchImg, 3000);
+        }
+        function stopTimer() {
+            clearInterval(timer);
+        }
+        container.hover(stopTimer, startTimer);
+        startTimer();
+    });
+
+    $("#getText1").on("click", () => {
+        $("#textbox").text("글자 입력 테스트");
+        const req = $.ajax("data.json");
+        req.done((data, status) => {
+            // JSON.parse(data).forEach((d) => $("#textbox").append('<br/>' + d.name));
+            data.forEach((d) => $("#textbox").append('<br/>' + d.name));
+        })
+    });
+
+    $("#getText2").on("click", () => {
+        const tb = $("<table />");
+        tb.append($("<tr />").append(
+            $("<th />").text("이름"),
+            $("<th />").text("아이디"),
+            $("<th />").text("학과"),
+            $("<th />").text("수강과목"),
+        ))
+        const req = $.get("data.json", (data, status) => {
+            data.forEach((d, i) => {
+                const row = $("<tr />").append(
+                    $("<td />").text(d.name),
+                    $("<td />").text(d.id),
+                    $("<td />").text(d.department),
+                    $("<td />").text(d.class.join(", ")),
+                );
+                tb.append(row);
+            });
+        });
+        $("#textbox").html(tb);
+    });
+
+    const req = $.ajax({
+        url: "/rss",
+        dataType: "xml"
+    });
+    req.done((data) => {
+        let items = $(data).find("item");
+        if (items.length > 0) {
+            items = items.slice(0, 5);
+            let uTag = $("<ul/>");
+            items.each(function() {
+                $("<li/>").html($(`<a/>`).html($(this).find("title").text()).attr(
+                    {"href": $(this).find("link").text(), "target": "_blank"})).appendTo(uTag)
+            });
+            uTag.appendTo("#news");
+        }
+    });
+    req.fail((jqXHR, textStatus) => {
+        alert(`failed: ${textStatus}`);
+    });
+})
+
+function update(j) {
+    let n = parseInt(j.text(), 10);
+    j.text(n + 1);
+}
+
+function flash() {
+    $("#off_test").show().fadeOut("slow");
+}
+
+function max_open(event) {
+    const maxwindow = window.open(event.data.url, "", event.data.winattributes);
+    maxwindow.moveTo(0, 0);
+    maxwindow.resizeTo(screen.availWidth, screen.availHeight);
+}
+
+function change_position(obj) {
+    let left = ($(window).width() - obj.width())/2;
+    let top = ($(window).height() - obj.height())/2;
+    obj.css({"top": top, "left": left});
+}
+
